039ca431b71c4d99eade81aa1e9142c24eaebe02
Illia Yatsentyuk <illiayatsentyuk@gmail.com>
2025-01-02 13:13:55 +0200

    add some changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,5 @@
-const { where } = require('sequelize');
 const Product = require('../models/product');
-
+const User = require('../models/user');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -14,14 +13,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
+  product
+    .save()
     .then((result) => {
-      console.log('Created product');
+      // console.log(result);
+      console.log('Created Product');
       res.redirect('/admin/products');
     })
     .catch((err) => {
@@ -35,39 +39,20 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findAll({
-    where: {
-      id: prodId,
-    },
-  })
+  Product.findById(prodId)
+    // Product.findById(prodId)
     .then((product) => {
-      if (!product[0]) {
+      if (!product) {
         return res.redirect('/');
       }
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product[0],
+        product: product,
       });
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then((products) => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -76,35 +61,44 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findAll({
-    where: {
-      id: prodId,
-    },
-  })
-    .then((product) => {
-      product[0].title = updatedTitle;
-      product[0].price = updatedPrice;
-      product[0].description = updatedDesc;
-      product[0].imageUrl = updatedImageUrl;
-      return product[0].save();
-    })
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
     .then((result) => {
-      console.log('UPDATED PRODUCT');
+      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 };
 
-exports.deleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.destroy({ where: { id: prodId } })
-    .then((result) => {
-      console.log('DELETED PRODUCT');
-      res.redirect('/admin/products');
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then((products) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then(() => {
+      return req.user.deleteItemFromCart(prodId);
+    })
+    .then((result) => {
+      console.log("DELETED");
+      res.redirect("/admin/products")
+    })
+    .catch((err) => console.log(err));
 };
--- util/database.js
+++ util/database.js
@@ -1,7 +1,28 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node-complete', 'root', 'I562530y2009', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const mongodb = require('mongodb');
 
-module.exports = sequelize;
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb://127.0.0.1:27017/')
+    .then((res) => {
+      console.log('Connected');
+      _db = res.db('shop');
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
